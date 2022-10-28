6264f03af1b53fb92aee816c1ada0206ef2d1008
Prerana Shukla <prerana.shukla@haworth.com>
2022-10-27 21:25:59 -0400

    created Hmepage and fix styling layout

---
+++ client/src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Footer() {
+    const footerStyle={
+        position: "relative",
+        left: 0,
+        bottom: 0,
+        width: "100%",
+        color: "rgb(158, 158, 158)",
+        textAlign:"center"
+    }
+    return (
+        <div class="footer mt-5" style={footerStyle}>
+            <p>(c) 2022 Popcorn and Movie</p>
+            <p>Created by:  </p>
+
+        </div>
+    )
+}
+
+export default Footer;
